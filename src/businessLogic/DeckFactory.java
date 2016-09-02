6f7d3e3facdff96b426e777d4709480b2bbbba3d
Miguel Ortiz <miaortizma@unal.edu.co>
2016-09-01 20:49:16 -0500

    DeckFactory: Clase diseñada para manejar la creación de barajas/mazos usar

---
+++ src/businessLogic/DeckFactory.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package businessLogic;
+
+import static businessLogic.DealingAssistant.shuffleDeck;
+import data.Card;
+import data.Deck;
+import data.Hand;
+
+/**
+ *
+ * @author One Poker
+ */
+public class DeckFactory {
+
+    public static Deck getDeck(String deckType) {
+        if (deckType.equalsIgnoreCase("DEALINGDECK")) {
+            Deck dealingDeck = new Deck("Dealing deck");
+            for (int i = 0; i < 52; i++) {
+                dealingDeck.addCard(new Card(i));
+            }
+            shuffleDeck(dealingDeck);
+            return dealingDeck;
+        } else if (deckType.equalsIgnoreCase("PLAYERHAND")) {
+            return new Hand();
+        } else {
+            return new Deck();
+
+        }
+
+    }
+
+}
