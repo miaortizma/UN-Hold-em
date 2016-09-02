4f327b1020e82f058bdf614370f100b7e3b70742
Miguel Ortiz <miaortizma@unal.edu.co>
2016-09-02 09:31:28 -0500

    Metodos para obtener arreglos de enteros, del rango de las cartas y del palo de las cartas

--- src/businessLogic/HandAnalyser.java
+++ src/businessLogic/HandAnalyser.java
@@ -14,11 +14,18 @@ import data.Hand;
  */
 public class HandAnalyser {
 
+    private String[] hands = {"4 of a Kind", "Straight Flush", "Straight", "Flush", "High Card",
+        "1 Pair", "2 Pair", "Royal Flush", "3 of a Kind", "Full House"};
+
     public static void pair(Hand hand) {
         //asume que recibe una mano ordenada
         for (int i = hand.getSize(); i > 0; i--) {
 
         }
+    }
+
+    public String[] getHands() {
+        return hands;
     }
 
     public static int[] rankFrequency(Hand hand) {
@@ -29,12 +36,30 @@ public class HandAnalyser {
         return frequency;
     }
 
+    public static int[] handSuits(Hand hand) {
+        int[] suits = new int[hand.getSize()];
+        for (int i = 0; i < hand.getSize(); i++) {
+            suits[i] = hand.getCard(i).getSuit();
+        }
+        return suits;
+    }
+
     public static int[] handRanks(Hand hand) {
-        int[] ranks = new int[5];
-        for (int i = 0; i < 5; i++) {
+        int[] ranks = new int[hand.getSize()];
+        for (int i = 0; i < hand.getSize(); i++) {
             ranks[i] = hand.getCard(i).getValue();
         }
         return ranks;
+    }
+
+    public static boolean allEqual(int[] x) {
+        int first = x[0];
+        for (int i = 0; i < x.length; i++) {
+            if (x[i] != first) {
+                return false;
+            }
+        }
+        return true;
     }
 
     /**
@@ -43,10 +68,8 @@ public class HandAnalyser {
      * @return indice de la carta màs alta
      */
     public static Card highCard(Hand hand) {
-        int maxCard = hand.getCard(0).getValue(), cardIndex = 0, cardValue;
+        int maxCard = hand.getCard(0).getValue(), cardIndex = 0;
         for (int i = 0; i < hand.getSize(); i++) {
-            cardValue = hand.getCard(i).getValue();
-
             if (hand.getCard(i).getValue() > maxCard) {
                 cardIndex = i;
             }
