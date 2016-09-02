5add5b52c9d3ba957eddf20532429c1bfe1b010e
Miguel Ortiz <miaortizma@unal.edu.co>
2016-09-02 04:44:11 -0500

    Encapsulado

--- src/data/Hand.java
+++ src/data/Hand.java
@@ -5,21 +5,49 @@
  */
 package data;
 
+
 /**
  *
  * @author OnePoker UN Estudiante
  */
 public class Hand extends Deck {
 
-    private Player player;
-
+    private String rank;
+    private int rankLevel;
     public Hand(String name) {
         super(name);
     }
 
-    public Hand(String name, Player player) {
-        super(name);
-        this.player = player;
+   
+  
+    /**
+     * @return the rank
+     */
+    public String getRank() {
+        return rank;
     }
+
+    /**
+     * @param rank the rank to set
+     */
+    public void setRank(String rank) {
+        this.rank = rank;
+    }
+
+    /**
+     * @return the rankLevel
+     */
+    public int getRankLevel() {
+        return rankLevel;
+    }
+
+    /**
+     * @param rankLevel the rankLevel to set
+     */
+    public void setRankLevel(int rankLevel) {
+        this.rankLevel = rankLevel;
+    }
+    
+    
 
 }
