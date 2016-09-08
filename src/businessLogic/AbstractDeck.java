ea833015bd9cef275aa010c0e462840bd65bb6b8
MIGUEL ORTIZ <maortizm99@gmail.com>
2016-09-07 21:48:03 -0500

    Added abstract card list handler

---
+++ src/businessLogic/AbstractDeck.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package businessLogic;
+
+import data.Card;
+import java.util.List;
+
+/**
+ *
+ * @author Miguel Angel
+ */
+public abstract class AbstractDeck {
+
+    private List<Card> cards;
+
+}
